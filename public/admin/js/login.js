3706f12e93417c3e7eede49831c3c75d30cd8a0b
2280350623 <2280350623@qq.com>
2019-03-18 16:28:40 +0800

    登录-静态页面完成

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,12 +1,6 @@
 $(function () {
     var $form = $('#form');
     $form.bootstrapValidator({
-        // 配置图标
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
         //校验
         fields: {
             //用户名
@@ -16,35 +10,64 @@ $(function () {
                     //非空校验
                     notEmpty: {
                         //为空时提示
-                        message:"用户名不能为空"
+                        message: "用户名不能为空"
                     },
                     //长度校验
                     stringLength: {
                         min: 2,
                         max: 6,
-                        message:"用户名长度须为2-6位"
+                        message: "用户名长度须为2-6位"
                     },
                     // callback
                 }
             },
             //密码
-            paddword: {
+            password: {
                 //校验规则
                 validators: {
                     //非空校验
                     notEmpty: {
                         //为空时提示
-                        message:"密码不能为空"
+                        message: "密码不能为空"
                     },
                     //长度校验
                     stringLength: {
                         min: 6,
                         max: 12,
-                        message:"密码长度须为6-12位"
+                        message: "密码长度须为6-12位"
                     }
                     // callback
                 }
             }
-        }
+        },
+        // 配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+    })
+
+    // 给表单注册校验成功的事件
+    $form.on('success.form.bv', function (e) {
+        //阻止浏览器默认行为
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',
+            data: $form.serialize(),
+            success: function (info) {
+                if (info.error === 1000) {
+                    alert('用户名不正确')
+                }
+                if (info.error === 1001) {
+                    alert('密码错误')
+                }
+                if (info.success) {
+                    // 登录成功
+                    location.href = 'index.html'
+                }
+            }
+        })
     })
 })
