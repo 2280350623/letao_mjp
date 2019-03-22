2feeb474ff5ec05252b65242eb50088b258f77af
2280350623 <2280350623@qq.com>
2019-03-22 11:18:19 +0800

    用户管理页面完成

--- public/admin/js/user.js
+++ public/admin/js/user.js
@@ -13,22 +13,43 @@ $(function () {
                 pageSize: pageSize
             },
             success: function (info) {
-                console.log(info);
-                var html=template('user_tpl', info);
-                $('tbody').html(html);
+                // console.log(info);
+                $('tbody').html(template('user_tpl', info));
                 // 分页
                 paginator(info, render);
             }
         })
     }
 
-    // 禁止和启用功能
+    // 禁止和启用模态框
     $('tbody').on('click','.btn',function(){
         // 弹出模态框
         $('#userModal').modal('show');
         // 获取用户的id及按钮状态
-        console.log($(this).parent());
+        // console.log($(this).parent());
         id=$(this).parent().data('id');
-        isDelete=$(this).text('启用')?1:0
+        isDelete=$(this).hasClass('btn-success')?1:0
+    })
+
+    // 模态框功能实现
+    $('.comply').on('click',function(){
+        // 发送ajax请求
+        $.ajax({
+            type:'post',
+            url:'/user/updateUser',
+            data:{
+                id:id,
+                isDelete:isDelete
+            },
+            success:function(info){
+                console.log(Math.ceil(id / pageSize));
+                if(info.success){
+                    //关闭模态框
+                    $('#userModal').modal('hide')
+                    // 重新渲染表格
+                    render(Math.ceil(id / pageSize))
+                }
+            }
+        })
     })
 })
